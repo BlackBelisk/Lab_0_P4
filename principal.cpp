ec0632529b311810b4e7da84900abef9a9f4b0c0
TerevintoMartin <101746971+TerevintoMartin@users.noreply.github.com>
2023-03-13 13:26:27 -0300

    Update principal.cpp

--- principal.cpp
+++ principal.cpp
@@ -10,9 +10,16 @@
 using namespace std;
 int main(){
 
+    /*parte a*/  
+    Libro libro1("Nacidos de la bruma: El imperio final", 2022, Roto, "Brandon Sanderson", 688);
+    Libro libro2("Las Malas", 2022, Nuevo, "Camila Sosa Villada", 240);
+    Libro libro3("El cocodrilo al que no le gustaba el agua", 2016, Roto, "Gemma Merino", 32);
+    /*parte b*/
+    JuegoMesa juego1("Juego Uno", 2022, Roto, 7, 10);
+    JuegoMesa juego2("Mazo de Cartas", 2019, Nuevo, 7, 4);
+    JuegoMesa juego3("Dados", 2020, Roto, 2, 6);
+    
     Child Maria("María Laura", 10, "Nueva Palmira 1521", "099298190");
     Child Alex("Alex", 5, "Humberto Primo 1501", "29094141");
-    Libro libro1("Las Malas", 2022, Nuevo, "Camila Sosa Villada", 240); 
-    JuegoMesa juego1("Juego Uno", 2022, Roto, 7, 10);
     return 0;
 };
